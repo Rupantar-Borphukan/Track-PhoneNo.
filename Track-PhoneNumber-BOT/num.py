8056bba5c0e80c447bfc7bb51002bb019a21679c
Rupantar-Borphukan <94108574+Rupantar-Borphukan@users.noreply.github.com>
2021-12-07 18:35:27 +0530

    Update num.py

--- Track-PhoneNumber-BOT/num.py
+++ Track-PhoneNumber-BOT/num.py
@@ -1,2 +1,5 @@
-# number = "+917532423423"
+#        Try giving any phone number in  ' number = " Cut the old number And give what you want to give " ,
+#        Like this ---> number = "+917532423423" '
+
+
 number = "+916005849396"
